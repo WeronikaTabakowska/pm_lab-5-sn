4a28f690af6afbddba132719dd420ed55e3cb27d
„Weronika <weronika.tabakowska@student.wat.edu.pl>
2021-01-25 10:55:05 +0100

    Wykonano zadanie 3.2.2 –sterowanie ekranami

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,32 +1,55 @@
 #include <LiquidCrystal.h>
 
-//Inicjalizacja obiektu klasy LiquidCrystal: RS, E, DB4, DB5, DB6, DB7
+#define buttonUp    11
+#define buttonDown  13
 LiquidCrystal lcd(9, 8, 7, 6, 5, 4);
-int menu=1; //Numer aktualnie wyświetlanego ekranu
+int menu=1;
+bool psButtonUp = LOW;
+bool psButtonDown = LOW;
 
-void setup(void) {
-  lcd.begin(16, 2); //Ustawienie liczby kolumn i wierszy
+void changeMenu(void){
+  if(digitalRead(buttonUp) == HIGH && psButtonUp == LOW){
+    psButtonUp = HIGH;
+    menu=menu+1;
+    if(menu>3){
+      menu=1;
+    }
+  }
+  if(digitalRead(buttonDown) == HIGH && psButtonDown == LOW) {
+    psButtonDown = HIGH;
+    menu=menu-1;
+    if(menu<1){
+      menu=3;
+    }
+  }
+  psButtonUp = digitalRead(buttonUp);
+  psButtonDown = digitalRead(buttonDown);
+}
+
+void setup(void){
+  lcd.begin(16, 2);
+  pinMode(buttonUp, INPUT_PULLUP);
+  pinMode(buttonDown, INPUT_PULLUP);
 }
 
 void loop(void) {
-  dispMenu(); //Wyświetlenie menu
-  delay(1000);
-  menu++; //Zmiana ekranu menu
-  if(menu>3) menu=1;
+  dispMenu();
+  changeMenu();
 }
-void dispMenu (void) {
-  switch(menu) {
-    case 1:
-        lcd.setCursor(0,0);
-        lcd.print("Menu 1");
-        break;
-    case 2:
-        lcd.setCursor(0,0);
-        lcd.print("Menu 2");
-        break;
-    case3:
-        lcd.setCursor(0,0);
-        lcd.print("Menu 3");
-        break;
+
+void dispMenu(void){
+  switch(menu){
+  case 1:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 1");
+    break;
+  case 2:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 2");
+    break;
+  case 3:
+    lcd.setCursor(0,0);
+    lcd.print("Menu 3");
+    break;
   }
 }
